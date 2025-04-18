a7ea054a333bbe1ea969b48a299f5403364ec973
DuyCoder <duynv.it.052@gmail.com>
2025-04-18 10:09:40 +0700

    bai_3_operator

---
+++ Bai_3_Operator/Bai_3_Operator/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Bai_3_Operator;
+
+class Program
+{
+     static void Main(string[] args)
+     {
+          float a = 5;
+          int b = 4;
+          Console.WriteLine("a + b = {0}" , a + b);
+          Console.WriteLine("a - b = {0}" , a - b);
+          Console.WriteLine("a * b = {0}" , a * b);
+          Console.WriteLine("a / b = {0}" , a / b);
+          Console.WriteLine("a % b = {0}" , a % b);
+
+          float result = a + b * 2;
+          Console.WriteLine("result: " + result); // = 13
+          float result2 = 6 / 2 + 3 * 2;
+          Console.WriteLine("result2: " + result2); // = 9
+          
+          
+
+     }
+}
