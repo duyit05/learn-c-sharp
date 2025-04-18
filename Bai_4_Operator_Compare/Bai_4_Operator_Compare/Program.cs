71342bf022fbbb67a0d0fbe61c9d982e4c236624
DuyCoder <duynv.it.052@gmail.com>
2025-04-18 10:34:42 +0700

    bai_4_operator_compare

---
+++ Bai_4_Operator_Compare/Bai_4_Operator_Compare/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Bai_4_Operator_Compare;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 5;
+        int b = 6;
+        Console.WriteLine($"{a} > {b} is {a > b}");
+        Console.WriteLine($"{a} < {b} is {a < b}");
+        Console.WriteLine($"{a} = {b} is {a == b}");
+        Console.WriteLine($"{a} >= {b} is {a >= b}");
+        Console.WriteLine($"{a} <= {b} is {a <= b}");
+        Console.WriteLine($"{a} != {b} is {a != b}");
+
+        bool result;
+        bool result1;
+        bool result2;
+
+        bool z = true;
+        bool x = false;
+
+        result = z && x;
+        result1 = z || x;
+
+        Console.Write(result);
+        Console.Write(result1);
+
+    }
+}
