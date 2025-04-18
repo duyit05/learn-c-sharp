0e6b90bb78d872f012d5401e7ab9c09c4fa39409
DuyCoder <duynv.it.052@gmail.com>
2025-04-18 09:44:54 +0700

    bai_2_variable

---
+++ Bai_2_Variable/Bai_2_Variable/Bai_2_Variable/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace  Bai_2_Variable;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Title in terminal
+        Console.Title = "Variable";
+        // Clear all content pre
+        Console.Clear();
+        // Change text color
+        Console.ForegroundColor = ConsoleColor.Green;
+        // Change background color
+        Console.BackgroundColor = ConsoleColor.White;
+
+        Console.Write("Enter full name: ");
+        string fullName = Console.ReadLine();
+        Console.WriteLine("Hello: " + fullName);
+        float a, b;
+        string input;
+        Console.Write("input a: ");
+        // type casting
+        input = Console.ReadLine();
+        a = float.Parse(input);
+        a = Convert.ToSingle(input);
+        
+        Console.Write("input b: ");
+        input = Console.ReadLine();
+        b = float.Parse(input);
+        
+        Console.Write("number a = {0} , number b = {1}",a,b);
+    }
+}
