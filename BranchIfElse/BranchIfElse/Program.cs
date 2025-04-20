aad494853bc1d1063128896d32ecc39f8e36b08d
DuyCoder <duynv.it.052@gmail.com>
2025-04-20 13:52:09 +0700

    bai_5_branch_if_else

---
+++ BranchIfElse/BranchIfElse/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace BranchIfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("input number a: ");
+            float a = float.Parse(Console.ReadLine());
+            if (a < 5.0)
+            {
+                Console.Write("Hoc luc yeu");
+            }
+            else if (a >= 5.0 && a <= 6.5)
+            {
+                Console.Write("Hoc luc trung binh");
+            }
+            else if (a >= 6.5 && a <= 8.0)
+            {
+                Console.Write("Hoc luc kha");
+            }
+            else if (a >= 8.0 && a <= 10)
+            {
+                Console.Write("Hoc luc gioi");
+            }
+            else
+            {
+                Console.Write("Hoc luc kem");
+            }
+        }
+    }
+}
