aadcee80a4af2d4e19b66391374dfebd528696f7
DuyCoder <duynv.it.052@gmail.com>
2025-04-26 14:04:58 +0700

    bai_8_string_string_builder

---
+++ StringAndStringBuidler/StringAndStringBuidler/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace StringAndStringBuilder;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string notification = "****Nguyen Van Duy, xin chao cac ban";
+        notification = notification.Trim('*');
+        int length = notification.Length;
+
+       
+        Console.WriteLine(notification);
+        for (int i = 0; i < length; i++)
+        {
+            char c = notification[i];
+            Console.WriteLine($"chi so {i} la ky tu: {c}");
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.Append("Xin");
+        stringBuilder.Append(" chao cac ban");
+        stringBuilder.Replace("Xin chao", "Cho mung");
+
+        String result = stringBuilder.ToString();
+        Console.WriteLine(result);
+    }
+}
